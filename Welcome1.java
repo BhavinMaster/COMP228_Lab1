715e799b427837d6a8afbefb9a815990eed2b562
BhavinMaster <bmaster@my.centennialcollege.ca>
2016-09-09 20:10:38 -0400

    Initial Commit
    ABC

---
+++ Welcome1.java
@@ -0,0 +1,9 @@
+// This is Class Welcome!
+public class Welcome1
+{
+	// Main Method
+	public static void main(String args[]){
+		// Display Welcome Message on Console
+		System.out.printf("%s%n%s","welcome to","Java Programming");
+	}// End Of Main Method
+} // End Of Class
